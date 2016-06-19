54fe04d4feeab6986a095d4029a22b876db0687e
Veera <veera.k.sundar@gmail.com>
2016-06-19 10:00:45 -0700

    Fix current directory detection logic.

--- src/cli.js
+++ src/cli.js
@@ -4,9 +4,10 @@
 
 const fs = require('fs');
 const path = require('path');
-const EXCLUSION_LIST = ['.git', 'node_modules', '.sass-cache'];
+
+let _walk = require('./js/walk');
 let textree = require('./js/textree');
-let DEFAULT_FOLDER = __dirname;
+let DEFAULT_FOLDER = process.cwd();
 
 function _init() {
     let params = _getCliParams();
@@ -42,30 +43,6 @@ function _getFolderToWalk(params) {
     return folder || DEFAULT_FOLDER;
 }
 
-function _walk(folder, data) {
-    let folderContents = fs.readdirSync(folder);
-    let noOfContents = folderContents.length;
-    let folderName = path.basename(folder);
-
-    data.name = folderName;
-    data.contents = [];
-
-    if (EXCLUSION_LIST.indexOf(folderName) != -1) {
-        return data;
-    }
-
-    for (let i = 0; i < noOfContents; i++) {
-        let content = folderContents[i];
-        let contentPath = path.join(folder, content);
-        let isFolder = fs.lstatSync(contentPath).isDirectory();
-        if (isFolder) {
-            data.contents.push(_walk(contentPath, {}));
-        } else {
-            data.contents.push(content);
-        }
-    }
-    return data;
-}
 
 function _usage(input) {
     console.log('"' + input + '" is not a directory. Exiting.');
